39205fe9a79a59d72483ff76c31cc4b31375d409
Max Alva <alva85@gmail.com>
2018-08-21 16:12:52 -0500

    ...

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    googleApiKey: 'AIzaSyBAq6jOdhI1F0U79L39w0dF5Kz4Ohj92sM',
-    openWeatherApiKey: '1937578dfa4e75747ec319edcfc520ef'
+    googleApiKey: '',
+    openWeatherApiKey: ''
 };
